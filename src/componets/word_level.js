84486ec12aa76280bea5967d59eb247bd5008f89
begosustatx <bego.sustatxa1.d@gmail.com>
2022-03-16 19:17:56 +0100

    cleanup

--- src/componets/word_level.js
+++ src/componets/word_level.js
@@ -1,21 +1,11 @@
 import React from 'react';
 import { useEffect, useState } from 'react'
-import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/solid'
 import ReactHtmlParser, {
-    convertNodeToElement,
     processNodes
 } from "react-html-parser";
-export default function Example({ object }) {
+export default function Example() {
 
-    const type = "word"
-    const [words, setWords] = useState([])
-    const [indexes, setIndexeses] = useState([])
-    const [effects, setEffects] = useState([])
-    const [objects, setObject] = useState([])
-    const [index, setIndex] = useState(0)
-    const [displayed, setdisplayed] = useState([])
     const [html, sethtml] = useState('')
-    const [level, setLevel] = useState('')
 
     const options = {
         decodeEntities: true,
@@ -26,42 +16,11 @@ export default function Example({ object }) {
         fetch('/process_text').then(res => res.json()).then(data => {
             console.log(data)
             sethtml(data.object)
-            //setWords(data.words.slice(0, 530))
-            //setIndexeses(data.indexes)
-            //setEffects(data.effects);
         });
     }, []);
-    /*
-    
-        function cutObject(data) {
-            let index = 0
-            let count = 0
-            let object_array = []
-            let temp_array = []
-            for (var i = 0; i < data.length; i++) {
-                console.log("here")
-                count = count + data[i].words.length
-                if (count <= 200) {
-                    temp_array = ([...temp_array, data[i]])
-                }
-                else {
-                    object_array = ([...object_array, temp_array])
-                    console.log("object_array", object_array)
-                    temp_array = []
-                    count = data[i].words.length
-                    temp_array = ([...temp_array, data[i]])
-                    index = index + 1
-                }
-                console.log(temp_array)
-    
-            }
-            setdisplayed(object_array[0])
-            setObject(object_array)
-        }*/
 
     function transform(node, index) {
-        // return null to block certain elements
-        // don't allow <span> elements
+        //TODO: maybe add more css to other elements?
         if (node.type === "tag" && node.attribs.class === "adj" && node.name === "span") {
             return (
                 <span
@@ -108,15 +67,6 @@ export default function Example({ object }) {
             });
     }
 
-    function onClickNext() {
-        setdisplayed(objects[index + 1])
-        setIndex(index + 1)
-    }
-
-    function onClickPrevious() {
-        setdisplayed(objects[index - 1])
-        setIndex(index - 1)
-    }
     return (
         <div className="ml-10 text-4xl tracking-wide leading-relaxed">
             <div >{ReactHtmlParser(html, options)}</div>
