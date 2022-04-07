7090d2f128273561964343da1b18870089ff163b
begosustatx <bego.sustatxa1.d@gmail.com>
2022-04-07 10:37:07 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,11 +4,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Main from './componets/main'
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route exact path="/" element={<Main />} />
-      </Routes>
-    </Router>
+    <Main />
   );
 }
 
